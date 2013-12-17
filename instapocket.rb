4934792475293a613b93e79485c5ce0c1d58194b
Luís Lago <luisalveslago@gmail.com>
2013-12-17 10:17:19 +0000

    create title

--- instapocket.rb
+++ instapocket.rb
@@ -4,11 +4,10 @@ require "open-uri"
 
 puts "URL: "
 remote_full_url = gets.chomp #link to be parsed
-puts "File name" 
-local_file_name = gets.chomp #the name of the file where the text will be copied
 remote_data = Nokogiri::HTML(open(remote_full_url))   
 
-my_local_filename = local_file_name + ".txt" # 
+#
+my_local_filename = remote_data.css("title").text + ".txt" # the name of the file where the text will be copied will be the title of the article
 my_local_file = open(my_local_filename, "w") # creates the .txt file where the text will be copied
 my_local_file.write(remote_full_url) #copies the URL to the file
 my_local_file.write("
